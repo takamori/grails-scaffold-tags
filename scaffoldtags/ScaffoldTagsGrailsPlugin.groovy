afb8b4aabecd9473bb0159255d85b80c58874ddd
github@dtakamori.com <daiji@dell-desktop.example.com>
2009-09-10 23:02:16 -0400

    Updated copyright notices

--- scaffoldtags/ScaffoldTagsGrailsPlugin.groovy
+++ scaffoldtags/ScaffoldTagsGrailsPlugin.groovy
@@ -1,4 +1,4 @@
-/* Copyright 2007 Daiji Takamori
+/* Copyright 2007-2009 Daiji Takamori
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
